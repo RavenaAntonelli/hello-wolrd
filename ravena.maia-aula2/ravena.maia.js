5c2d86e16cac5a8f63a8d6ab318a86889d1230c8
RavenaAntonelli <87223198+RavenaAntonelli@users.noreply.github.com>
2021-08-26 08:48:47 -0300

    Add files via upload

---
+++ ravena.maia-aula2/ravena.maia.js
@@ -0,0 +1,19 @@
+alert("Seja bem vinda ao meu site!")
+
+function A(x) {
+    function B(y) {
+          alert(x + y );
+    }
+    B(96);
+ }
+ A(87);
+  
+function C(w) {
+    function D(z) {
+          alert(z*w );
+    }
+    D(5);
+ }
+ C(8);
+ var lista = [ "Banana", "Granola", "Chocolate","Manga" ];
+ alert("Lista:" +lista)
